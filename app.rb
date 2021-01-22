381e9f23f6581706a908ed305ad2e3c947fae084
Josh Thompson <thompsonjoshd@gmail.com>
2021-01-22 16:25:26 -0700

    step.... 6? reads/returns query params

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  JSON.generate(env)
+  response = {
+    message: "it me, a response!",
+    given_query_params: request.env["QUERY_STRING"],
+    HTTP_USER_AGENT: request.env["HTTP_USER_AGENT"]
+  }
+  response.to_json
 end
