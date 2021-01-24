a445f62a6a3ac658e0622006eca6cd5592c72c6a
Josh Thompson <thompsonjoshd@gmail.com>
2021-01-23 19:36:53 -0700

    Now serves random data from CSV to frontend

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra'
 require 'json'
 
 get '/?' do
-  File.read('index.html')
+  current_time = Time.now.strftime("%r %Z")
+  @message = "It is #{current_time} and all is well!"
+  @random_row = get_random_row
+  erb :index, locals: { message: @message, 
+                        random_row: @random_row
+                      }
+end
+
+def get_random_row
+  file = File.open('data/20210124030110_4746.csv')
+  lines = file.readlines
+  lines.sample
 end
