9f7c651d7b633eae3393dd19b42a23885a840fd6
Josh Thompson <thompsonjoshd@gmail.com>
2021-01-23 18:12:42 -0700

    basic styling

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
 require 'sinatra'
 require 'json'
 
-get '/' do
-  response = {
-    message: "it me, a response!",
-    given_query_params: request.env["QUERY_STRING"],
-    HTTP_USER_AGENT: request.env["HTTP_USER_AGENT"]
-  }
-  response.to_json
+get '/?' do
+  File.read('index.html')
 end
